3745873f022ab60afefcc3fd4e0b09a9c68485c0
BRUNO LEONARDO SILVA <bruno.lsilva70@senacsp.edu.br>
2025-08-26 20:03:26 -0300

    First commit

---
+++ src/test/java/br/com/senacsp/tads/stads4ma/libray/LibrayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senacsp.tads.stads4ma.libray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
